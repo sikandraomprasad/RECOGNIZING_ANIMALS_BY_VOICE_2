3906c8bf799adbc0111088768425405ec7fef326
sikandraomprasad <89696118+sikandraomprasad@users.noreply.github.com>
2023-04-22 17:34:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function startClassification(){
+    navigator.mediaDevices.getUserMedia({audio : true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/kA4F-80yW/model.json',modelReady);
+}
+function modelReady(){
+    classifier.classify(gotResults);
+}
+function gotResults(error, results){
+   
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+    }
+}
